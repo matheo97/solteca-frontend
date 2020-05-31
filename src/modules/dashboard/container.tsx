cd6bc366a790d867966027d4c616fa40a9a80627
Mateo Salazar <mateosalazar@Mateos-MacBook-Pro.local>
2020-05-30 21:32:56 -0500

    Graph created still needs polish

--- src/modules/dashboard/container.tsx
+++ src/modules/dashboard/container.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Parking from 'types';
 import { Card } from '../../components';
+import Graph from './graph';
 import bankrupt from '../../images/dashboard/bankrupt.svg';
 import earnings from '../../images/dashboard/earnings.svg';
 import media from '../../images/dashboard/media.svg';
@@ -18,14 +19,36 @@ class Dashboard extends Component<Props, State> {
     this.state = {}
   }
   render() {
+    const data = [
+      {
+        quater: "Cuatrimestre 1",
+        cuatrimestre1: 16200000,
+        cuatrimestre1Color: "hsl(45, 70%, 50%)",
+      },
+      {
+        quater: "Cuatrimestre 2",
+        cuatrimestre2: 6200000,
+        cuatrimestre2Color: "hsl(89, 70%, 50%)",
+      },
+      {
+        quater: "Cuatrimestre 3",
+        cuatrimestre3: 10000000,
+        cuatrimestre3Color: "hsl(346, 70%, 50%)",
+      },
+    ]
     return (
       <div className='dashboard-wrapper'>
         <div className='upper-container'>
+          <div className='graph'>
+            <div className='title'>ventas por cuatrimestre:</div>
+            <div className='graph-container'>
+              <Graph 
+                data={data}
+              />
+            </div>
+          </div>
           <div className='upload-file'>
             upload-file
-          </div>
-          <div className='graph'>
-            graph 
           </div>
         </div>
         <div className='cards-container'>
@@ -35,7 +58,7 @@ class Dashboard extends Component<Props, State> {
             value={'13.000.000'}
             color={'red'}
           />
-          <Card 
+          <Card
             icon={earnings}
             title='dinero que nos deben a la fecha'
             value={'33.000.000'}
